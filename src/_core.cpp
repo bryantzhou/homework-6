e5455f719d0564ac604701b1ce59d952d53305a4
bryantzhou <yz1324@princeton.edu>
2023-12-15 13:43:46 -0500

    hw6

---
+++ src/_core.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "integrator.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m) {
+    py::class_<HarmonicOscillator>(m, "HarmonicOscillator")
+        .def(py::init<>())
+        .def("integrate", &HarmonicOscillator::integrate);
+}
